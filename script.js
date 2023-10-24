1c4c2a23004909b610d11b6c6e65376e577a3350
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-10-24 21:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded" function () {
+	let body = document.body;
+	let pera = document.createElement("p");
+	pera.innerText="DOM load success";
+	
+	
+})
